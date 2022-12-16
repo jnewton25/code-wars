f84036e8279666add3938eb8e3c49f564431953a
Justin Newton <justinmnewton25@gmail.com>
2022-12-16 09:48:54 -0600

    Create phone number

--- practice.js
+++ practice.js
@@ -82,3 +82,12 @@ function solution(str, ending){
   return (str.endsWith(ending) ? true : false);
 }
 */
+
+/* Create phone number
+Write a function that accepts an array of 10 integers (between 0 and 9), 
+that returns a string of those numbers in the form of a phone number.
+
+function createPhoneNumber(numbers){
+  return (`(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`);
+}
+*/
