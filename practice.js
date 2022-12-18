60739451f11d1d512a40b0b71f65d9aa333b55af
Justin Newton <justinmnewton25@gmail.com>
2022-12-18 07:57:15 -0600

    Square Every Digit

--- practice.js
+++ practice.js
@@ -193,3 +193,22 @@ function disemvowel(str) {
   return str.replace(/[aeiou]/ig, "");
 }
 */
+
+/* Square Every Digit
+ Welcome. In this kata, you are asked to square every digit of a number and 
+ concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, 
+because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+
+function squareDigits(num){
+  let arr = ("" + num).split("");
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] *= arr[i];
+  }
+  let int = Number(arr.join(''));
+  return int;
+}
+ */
