769c99fe6d20faff2cbe48b4aad34b8c0f10903b
Justin Newton <justinmnewton25@gmail.com>
2022-12-21 12:46:56 -0600

    Convert boolean

--- practice.js
+++ practice.js
@@ -227,3 +227,13 @@ function solution(str){
   return word;
 }
 */
+
+/* Convert boolean values to strings 'Yes' or 'No'.
+
+Complete the method that takes a boolean value and return a 
+"Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  return (bool === true ? "Yes" : "No");
+}
+*/
