26e3df04e76f7d3d09ee90042595cb6d109ce270
Justin Newton <justinmnewton25@gmail.com>
2022-12-20 21:13:41 -0600

    Reverse string

--- practice.js
+++ practice.js
@@ -212,3 +212,18 @@ function squareDigits(num){
   return int;
 }
  */
+
+/* Reversed Strings
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+function solution(str){
+  let word = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    word += str[i];
+  }
+  return word;
+}
+*/
