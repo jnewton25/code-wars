6a7b6c0d9f6f8e72412e34efb9a3f3a42aa4632c
Justin Newton <justinmnewton25@gmail.com>
2022-12-23 22:39:05 -0500

    Convert a Number to a String

--- practice.js
+++ practice.js
@@ -246,3 +246,14 @@ function opposite(number) {
   return number * -1;
 }
 */
+
+/* Convert a Number to a String!
+
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+function numberToString(num) {
+  return num.toString();
+}
+*/
