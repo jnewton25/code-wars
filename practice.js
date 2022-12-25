9a1f94cad04d82d7ded94586d97c3842d5716bf5
Justin Newton <justinmnewton25@gmail.com>
2022-12-25 07:43:39 -0500

    Highest and Lowest

--- practice.js
+++ practice.js
@@ -272,3 +272,14 @@ function removeChar(str){
   return x;
 };
 */
+
+/* Highest and Lowest
+
+In this little assignment you are given a string of space separated numbers, 
+and have to return the highest and lowest number.
+
+function highAndLow(numbers){
+  let arr = numbers.split(" ");
+  return `${Math.max(...arr)} ${Math.min(...arr)}`;
+}
+*/
