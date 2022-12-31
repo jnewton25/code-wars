99ef676367a7a28b4192237c4d2acca3701bd409
Justin Newton <justinmnewton25@gmail.com>
2022-12-31 14:58:07 -0600

    Counting sheep

--- practice.js
+++ practice.js
@@ -376,3 +376,21 @@ class SmallestIntegerFinder {
   }
 }
 */
+
+/* Counting sheep...
+
+Consider an array/list of sheep where some sheep may be missing from their place. 
+We need a function that counts the number of sheep present in the array (true means present).
+
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      count += 1;
+    } else {
+      continue;
+    }
+  }
+  return count;
+}
+*/
