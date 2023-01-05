9ff87f432e8d9ef930c769843806305a9794130b
Justin Newton <justinmnewton25@gmail.com>
2023-01-05 06:35:59 -0600

    Is n divisble by x and y

--- practice.js
+++ practice.js
@@ -445,3 +445,13 @@ function litres(time) {
   return Math.floor(time * .5);
 }
 */
+
+/* Is n divisible by x and y?
+
+Create a function that checks if a number n is divisible by two numbers x AND y. 
+All inputs are positive, non-zero numbers.
+
+function isDivisible(n, x, y) {
+  return (n % x === 0 && n % y === 0 ? true : false);
+}
+*/
