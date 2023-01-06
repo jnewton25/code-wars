1a6d87b685f893ea4e599f181df28d0cff7c8cb9
Justin Newton <justinmnewton25@gmail.com>
2023-01-06 10:46:26 -0600

    Convert a String to a Number

--- practice.js
+++ practice.js
@@ -455,3 +455,16 @@ function isDivisible(n, x, y) {
   return (n % x === 0 && n % y === 0 ? true : false);
 }
 */
+
+/* Convert a String to a Number!
+
+We need a function that can transform a string into a number. 
+What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly 
+valid representation of an integral number.
+
+const stringToNumber = function(str){
+  return eval(str);
+}
+*/
