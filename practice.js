1a6f27b263cf36b5939a524ba4c7493a78585a97
Justin Newton <justinmnewton25@gmail.com>
2023-01-08 10:57:58 -0600

    Convert number to reversed array of digits

--- practice.js
+++ practice.js
@@ -489,3 +489,15 @@ function findNeedle(haystack) {
   }
 }
 */
+
+/* Convert number to reversed array of digits
+
+Given a random non-negative number, you have to return the digits of this number within 
+an array in reverse order.
+
+function digitize(n) {
+  let myFunc = num => Number(num); 
+  var intArr = Array.from(String(n), myFunc);
+  return intArr.reverse();
+}
+*/
