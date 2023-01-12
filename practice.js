0bd8b241c5c19bc3225b956f02cdbf8b429a540a
Justin Newton <justinmnewton25@gmail.com>
2023-01-12 11:48:17 -0600

    Beginner Series #2 Clock

--- practice.js
+++ practice.js
@@ -533,3 +533,14 @@ function greet() {
   return `hello world!`;
 }
 */
+
+/* Beginner Series #2 Clock
+
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+function past(h, m, s){
+  return ((h * 3600000) + (m * 60000) + (s * 1000));
+}
+*/
