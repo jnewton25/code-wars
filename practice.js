9bf630044e283fb52d56c193c608254750ee5314
Justin Newton <justinmnewton25@gmail.com>
2023-01-17 09:40:29 -0600

    Convert a Boolean to a string

--- practice.js
+++ practice.js
@@ -604,3 +604,12 @@ function invert(array) {
   return arr;
 }
 */
+
+/* Convert a Boolean to a String
+
+Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+  return (b === true ? 'true' : 'false');
+}
+*/
