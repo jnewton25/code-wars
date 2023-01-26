dbedfb84cb2742afc41e5a54166887a21f464baa
Justin Newton <justinmnewton25@gmail.com>
2023-01-26 15:06:03 -0600

    Fake Binary

--- practice.js
+++ practice.js
@@ -687,3 +687,23 @@ function findAverage(array) {
   return total /= array.length;
 }
 */
+
+/* Fake Binary
+
+Given a string of digits, you should replace any digit below 5 with '0' and 
+any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+function fakeBin(x){
+  let y = '';
+  for(let i = 0; i < x.length; i++) {
+    if(x[i] >= 5) {
+      y += '1';
+    } else {
+      y += '0';
+    }
+  }
+  return y;
+}
+ */
