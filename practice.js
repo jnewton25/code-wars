89412baaa20996c78d48ac5420a08417d0ef0e5a
Justin Newton <justinmnewton25@gmail.com>
2023-01-27 16:15:09 -0600

    How good are you really

--- practice.js
+++ practice.js
@@ -707,3 +707,27 @@ function fakeBin(x){
   return y;
 }
  */
+
+/* How good are you really?
+
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the 
+average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and 
+compare your score!
+
+Return True if you're better, else False!
+
+function betterThanAverage(classPoints, yourPoints) {
+  let total = 0;
+  for(let i = 0; i < classPoints.length; i++) {
+    total += classPoints[i];
+  }
+  if((total / classPoints.length) < yourPoints) {
+    return true;
+  } else {
+    return false;
+  }
+}
+*/
