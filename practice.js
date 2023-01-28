8b9c999059f1a9b20bbf2882cd759134ddb94b60
Justin Newton <justinmnewton25@gmail.com>
2023-01-28 17:08:11 -0600

    Sum arrays

--- practice.js
+++ practice.js
@@ -731,3 +731,22 @@ function betterThanAverage(classPoints, yourPoints) {
   }
 }
 */
+
+/* Sum Arrays
+
+Write a function that takes an array of numbers and returns the sum of the numbers. 
+The numbers can be negative or non-integer. If the array does not contain any 
+numbers then you should return 0.
+
+function sum (numbers) {
+  var total = 0;
+  if (numbers.length > 0) {
+    for (let i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+  } else {
+    return 0;
+  }
+  return total;
+};
+*/
