9da601c0f3170cbf6215b69d9ec2a36a845ff1fb
Justin Newton <justinmnewton25@gmail.com>
2023-01-30 10:55:03 -0600

    Reduce but grow

--- practice.js
+++ practice.js
@@ -750,3 +750,19 @@ function sum (numbers) {
   return total;
 };
 */
+
+/* Beginner - Reduce but Grow
+
+Given a non-empty array of integers, return the result of multiplying the values 
+together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+  let total = 1;
+  for (let i = 0; i < x.length; i++) {
+    total *= x[i];
+  }
+  return total;
+}
+*/
