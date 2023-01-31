2987f363139d932dfcadbcd4891f6bf5c70b91c6
Justin Newton <justinmnewton25@gmail.com>
2023-01-31 17:19:02 -0600

    Get the middle character

--- practice.js
+++ practice.js
@@ -766,3 +766,20 @@ function grow(x){
   return total;
 }
 */
+
+/* Get the Middle Character
+
+You are going to be given a word. Your job is to return the middle character 
+of the word. If the word's length is odd, return the middle character. 
+If the word's length is even, return the middle 2 characters.
+
+function getMiddle(s)
+{
+  let half = ((s.length) / 2) - 1;
+  if (s.length % 2 === 0) {
+    return s.slice(half, half + 2);
+  } else {
+    return s.charAt(s.length/2);
+  }
+}
+*/
