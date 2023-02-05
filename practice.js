b6d77af867892272f27d2c9ae1a509f67c03f74c
Justin Newton <justinmnewton25@gmail.com>
2023-02-04 20:49:45 -0600

    Reversed words

--- practice.js
+++ practice.js
@@ -783,3 +783,16 @@ function getMiddle(s)
   }
 }
 */
+
+/* Reversed Words
+
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+function reverseWords(str){
+  const arr = str.split(" ");
+  arr.reverse();
+  return arr.join(" ");
+}
+*/
