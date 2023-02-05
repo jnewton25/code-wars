bdd79de1b0296c1c5c3d13c552f0f374d5c070ee
Justin Newton <justinmnewton25@gmail.com>
2023-02-05 08:00:01 -0600

    Reversed sequence

--- practice.js
+++ practice.js
@@ -796,3 +796,18 @@ function reverseWords(str){
   return arr.join(" ");
 }
 */
+
+/* Reversed sequence
+
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+  let arr = [];
+  for (let i = n; i > 0; i--) {
+    arr.push(i);
+  }
+  return arr;
+};
+*/
