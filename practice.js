777146d08408dd2dcdd9f5ccac359c3911edba0c
Justin Newton <justinmnewton25@gmail.com>
2023-02-07 06:38:11 -0600

    Simple multiplication

--- practice.js
+++ practice.js
@@ -836,3 +836,13 @@ function bmi(weight, height) {
   }
 }
 */
+
+/* Simple multiplication
+
+This kata is about multiplying a given number by eight if it is 
+an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+    return (number % 2 === 0 ? number * 8 : number * 9);
+}
+*/
