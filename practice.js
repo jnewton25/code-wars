862d5882590ca43b24f34c4d3f0a1bf93eafb788
Justin Newton <justinmnewton25@gmail.com>
2023-02-11 07:58:46 -0600

    DNA to RNA conversion

--- practice.js
+++ practice.js
@@ -886,3 +886,32 @@ function hero(bullets, dragons){
   return (bullets >= dragons * 2 ? true : false);
 }
 */
+
+/* DNA to RNA Conversion
+
+Deoxyribonucleic acid, DNA is the primary information storage molecule 
+in biological systems. It is composed of four nucleic acid bases Guanine 
+('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA 
+differs slightly from DNA its chemical structure and contains no Thymine. 
+In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+
+function DNAtoRNA(dna) {
+  let RNA = "";
+  for (let i = 0; i < dna.length; i++) {
+    if (dna.charAt(i) === "T") {
+      RNA += "U";
+    } else {
+      RNA += dna.charAt(i);
+    }
+  }
+  return RNA;
+}
+*/
