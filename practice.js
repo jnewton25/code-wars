2dab8985e94ff6f84b7721443cd3c23fe57525cc
Justin Newton <justinmnewton25@gmail.com>
2023-02-13 07:22:27 -0600

    Jenny's secret message

--- practice.js
+++ practice.js
@@ -930,3 +930,19 @@ var max = function(list){
     return Math.max(...list);
 }
 */
+
+/* Jenny's secret message
+
+Jenny has written a function that returns a greeting for a user. 
+However, she's in love with Johnny, and would like to greet him slightly 
+different. She added a special case to her function, but she made a mistake.
+
+Can you help her?
+
+function greet(name) {
+  if(name === "Johnny") {
+    return "Hello, my love!";
+  } else {
+  return ("Hello, " + name + "!");
+}}
+*/
