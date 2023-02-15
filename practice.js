ba2f6d590104faed34d9c11ff7f9a7dcaf06f583
Justin Newton <justinmnewton25@gmail.com>
2023-02-15 08:11:55 -0600

    Sentence smash

--- practice.js
+++ practice.js
@@ -965,3 +965,15 @@ function countBy(x, n) {
   return z;
 }
 */
+
+/* Sentence Smash
+
+Write a function that takes an array of words and smashes them together into a 
+sentence and returns the sentence. You can ignore any need to sanitize words or 
+add punctuation, but you should add spaces between each word. Be careful, 
+there shouldn't be a space at the beginning or the end of the sentence!
+
+function smash (words) {
+   return words.join(" ");
+};
+*/
