15f47f05bb183b8e6915c293a8d02819c8d66d11
Justin Newton <justinmnewton25@gmail.com>
2023-02-16 08:50:38 -0600

    If you can't sleep

--- practice.js
+++ practice.js
@@ -977,3 +977,22 @@ function smash (words) {
    return words.join(" ");
 };
 */
+
+/* If you can't sleep, just count sheep!!
+
+Given a non-negative integer, 3 for example, return a string with a 
+murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, 
+i.e. no negative integers.
+
+var countSheep = function (num){
+  let sent = "";
+  if (num > 0)
+    {for (let i = 1; i <= num; i++) {
+      sent += `${i} sheep...`;
+    }
+    return sent;
+  } else {
+    return "";
+  }
+}
+*/
