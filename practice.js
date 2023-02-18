c3688509b4d5106872d2fe273f152272c3ab8812
Justin Newton <justinmnewton25@gmail.com>
2023-02-18 07:29:38 -0600

    Array plus array

--- practice.js
+++ practice.js
@@ -1015,3 +1015,19 @@ function getAverage(marks){
   return Math.floor(total / marks.length);
 }
 */
+
+/* Array plus array
+
+I'm new to coding and now I want to get the sum of two arrays... 
+Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+  let arr = [...arr1, ...arr2], sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+*/
