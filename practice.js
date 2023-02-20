8ef0166213dd87dc8f5b7dcf4049c311e88f3548
Justin Newton <justinmnewton25@gmail.com>
2023-02-20 06:56:50 -0600

    Rock paper scissors

--- practice.js
+++ practice.js
@@ -1040,3 +1040,22 @@ function stringToArray(string){
   return string.split(" ");
 }
 */
+
+/* Rock Paper Scissors!
+
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+const rps = (p1, p2) => {
+  if (p1 === "rock" && p2 === "scissors" || 
+      p1 === "scissors" && p2 === "paper" ||
+      p1 === "paper" && p2 === "rock") {
+    return "Player 1 won!";
+  } else if (p2 === "rock" && p1 === "scissors" || 
+      p2 === "scissors" && p1 === "paper" ||
+      p2 === "paper" && p1 === "rock") {
+    return "Player 2 won!";
+  } else {
+    return "Draw!";
+  }
+};
+*/
