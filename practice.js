9ee3b3909a9c635a6eee9f0bac6ac74ec79fd97a
Justin Newton <justinmnewton25@gmail.com>
2023-02-21 09:26:35 -0600

    You can't code under pressure #1

--- practice.js
+++ practice.js
@@ -1059,3 +1059,12 @@ const rps = (p1, p2) => {
   }
 };
 */
+
+/* You Can't Code Under Pressure #1
+
+Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+  return i * 2;
+}
+*/
