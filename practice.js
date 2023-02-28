3e4f383c001afe0708dfacd082ad115c737d42bd
Justin Newton <justinmnewton25@gmail.com>
2023-02-28 12:37:26 -0600

    Grasshopper

--- practice.js
+++ practice.js
@@ -1162,3 +1162,23 @@ function bonusTime(salary, bonus) {
   return (bonus === true ? `\u00A3` + salary * 10 : `\u00A3` + salary);
 }
 */
+
+/* Grasshopper - Grade book
+
+Complete the function so that it finds the average of the three scores passed to it and 
+returns the letter value associated with that grade.
+
+function getGrade (s1, s2, s3) {
+  if (((s1 + s2 + s3) / 3) >= 90 && ((s1 + s2 + s3) / 3) <= 100) {
+      return 'A';
+  } else if (((s1 + s2 + s3) / 3) >= 80 && ((s1 + s2 + s3) / 3) < 90) {
+      return 'B';
+  } else if (((s1 + s2 + s3) / 3) >= 70 && ((s1 + s2 + s3) / 3) < 80) {
+      return 'C';
+  } else if (((s1 + s2 + s3) / 3) >= 60 && ((s1 + s2 + s3) / 3) < 70) {
+      return 'D';
+  } else {
+    return 'F';
+  }
+}
+*/
