baa173591050c9485d5e6fda5249eff9e5b0c027
Justin Newton <justinmnewton25@gmail.com>
2023-03-02 06:50:46 -0600

    Area or perimeter

--- practice.js
+++ practice.js
@@ -1193,3 +1193,13 @@ function greet (name, owner) {
   return (name === owner ? "Hello boss" : "Hello guest");
 }
 */
+
+/* Area or Perimeter
+
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+const areaOrPerimeter = function(l , w) {
+  return (l === w ? l * w : l + l + w + w);
+};
+*/
