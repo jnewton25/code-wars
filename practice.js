4c165b5fb9ea648f373a0a78d6a0258cd0f161f4
Justin Newton <justinmnewton25@gmail.com>
2023-03-15 05:28:31 -0500

    Will there be enough space

--- practice.js
+++ practice.js
@@ -1393,3 +1393,15 @@ function otherAngle(a, b) {
   return 180 - (a + b);
 }
 */
+
+/* Will there be enough space?
+
+Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. 
+With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not 
+enough space left on the bus! He wants you to write a simple program telling him if he will be able 
+to fit all the passengers.
+
+function enough(cap, on, wait) {
+  return (cap - (on + wait) > 0 ? 0 : (on + wait) - cap);
+}
+*/
