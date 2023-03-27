a755c5a8873b0ddf7212b8e69ea42821a85acbd3
Justin Newton <justinmnewton25@gmail.com>
2023-03-27 07:13:31 -0500

    Jumbo

--- practice.js
+++ practice.js
@@ -1555,3 +1555,15 @@ function goals (i, j, k) {
   return i+j+k;
 }
 */
+
+/* Jumbo
+function accum(str) {
+	var letters = str.split('');
+  var result = [];
+  for (var i = 0; i < letters.length; i++) {
+    result.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()));
+  }
+  return result.join('-');
+}
+
+*/
