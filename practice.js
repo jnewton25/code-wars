15082c79ab5f8ea692baee1f6d05884fc44ebbdd
Justin Newton <justinmnewton25@gmail.com>
2023-03-29 06:23:53 -0500

    Divisble by given number

--- practice.js
+++ practice.js
@@ -1575,3 +1575,21 @@ function square(n) {
   return n * n;
 }
 */
+
+/* Find numbers which are divisible by given number
+
+Complete the function which takes two arguments and returns all numbers which are divisible 
+by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+function divisibleBy(numbers, divisor){
+  let arr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % divisor === 0) {
+      arr.push(numbers[i]);
+    } else {
+      continue;
+    }
+  }
+  return arr;
+}
+*/
