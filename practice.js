fa631636ec0d502117f7e9debb32901d3d5e8a0d
Justin Newton <justinmnewton25@gmail.com>
2023-04-01 07:32:40 -0500

    Is it a palindrome

--- practice.js
+++ practice.js
@@ -1633,3 +1633,12 @@ function sumArray(array) {
   }
 }
 */
+
+/* Is it a palindrome?
+
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+function isPalindrome(x) {
+  return (x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false);
+}
+*/
