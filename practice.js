b8905f2a948704a599bae6644bc68dfcc2bc2792
Justin Newton <justinmnewton25@gmail.com>
2023-04-04 05:57:43 -0500

    Debug sayHello

--- practice.js
+++ practice.js
@@ -1675,3 +1675,13 @@ function testEven(n) {
   return n % 2 === 0 ? true : false;
 }
 */
+
+/* Grasshopper - Debug sayHello
+
+The starship Enterprise has run into some problem when creating a program to greet everyone as they 
+come aboard. It is your job to fix the code and get the program working again!
+
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
+*/
