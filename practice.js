9c37c9cb3cc2354192503e00179284b7092332dc
Justin Newton <justinmnewton25@gmail.com>
2023-04-08 10:50:23 -0500

    Contamination #1

--- practice.js
+++ practice.js
@@ -1730,3 +1730,19 @@ function fixTheMeerkat(arr) {
   return arr.reverse();
 }
 */
+
+/* Contamination #1 -String-
+
+An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+function contamination(text, char){
+  return (text != "" && char != "" ? text.replace(text, char.repeat(text.length)) : "");
+}
+*/
