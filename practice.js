baf11f1d84fd68b4a2f8a6d3df8414e1a4d0624f
Justin Newton <justinmnewton25@gmail.com>
2023-04-12 05:51:57 -0500

    Grasshopper - terminal game #1

--- practice.js
+++ practice.js
@@ -1793,3 +1793,16 @@ function strCount(str, letter){
   return count;
 }
 */
+
+/* Grasshopper - Terminal Game #1
+
+In this first kata in the series, you need to define a Hero prototype to be used in a terminal game.
+
+function Hero (name) {
+  this.name = name || "Hero";
+  this.position = "00";
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
+*/
