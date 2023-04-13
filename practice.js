a09dc7d8d676964a439a3c24d9959dc182eba757
Justin Newton <justinmnewton25@gmail.com>
2023-04-13 10:52:05 -0500

    Fun with ES6 classes #1

--- practice.js
+++ practice.js
@@ -1806,3 +1806,24 @@ function Hero (name) {
   this.experience = 0;
 }
 */
+
+/* Fun with ES6 Classes #1 - People, people, people
+
+Define a class Person
+
+class Person {
+  constructor(firstName, lastName, age, gender) {
+    this.firstName = firstName || "John";
+    this.lastName = lastName || "Doe";
+    this.age = age || 0;
+    this.gender = gender || "Male";
+  }
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+Person.greetExtraTerrestrials = function(raceName) {
+  return `Welcome to Planet Earth ${raceName}`;
+}
+*/
