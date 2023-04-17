a1ef57f6bcb591e3699b96aac6d85ee68205bd31
Justin Newton <justinmnewton25@gmail.com>
2023-04-17 08:22:14 -0500

    Basic: making six toast

--- practice.js
+++ practice.js
@@ -1874,3 +1874,13 @@ function addLength(str) {
   return arr;
 }
 */
+
+/* BASIC: Making Six Toast.
+
+You are going to make toast fast, you think that you should make multiple pieces of toasts and once. 
+So, you try to make 6 pieces of toast.
+
+function sixToast(num) {
+  return (num >= 6 ? num - 6 : num);
+}
+*/
