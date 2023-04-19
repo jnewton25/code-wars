e363e5357f5da82217b23f1bcd362d6cfbdec274
Justin Newton <justinmnewton25@gmail.com>
2023-04-19 07:51:12 -0500

    Keep the order

--- practice.js
+++ practice.js
@@ -1894,3 +1894,15 @@ function helloWorld() {
   console.log(str);
 }
 */
+
+/* Keep the Order
+
+Your job here is to write a function (keepOrder in JS/CoffeeScript, keep_order in Ruby/Crystal/Python, 
+keeporder in Julia), which takes a sorted array ary and a value val, and returns the lowest index 
+where you could insert val to maintain the sorted-ness of the array. The input array will always 
+be sorted in ascending order. It may contain duplicates.
+
+function keepOrder(ary, val) {
+  return ary.filter(a => a < val).length;
+}
+*/
