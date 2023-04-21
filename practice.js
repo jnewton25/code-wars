6a91bbd779ce2d6001ccfdb6dbd0897a675d31e1
Justin Newton <justinmnewton25@gmail.com>
2023-04-21 09:01:39 -0500

    Filter out the geese

--- practice.js
+++ practice.js
@@ -1906,3 +1906,19 @@ function keepOrder(ary, val) {
   return ary.filter(a => a < val).length;
 }
 */
+
+/* Filter out the geese
+
+Write a function that takes a list of strings as an argument and returns a filtered list 
+containing the same elements but with the 'geese' removed.
+
+The geese are any strings in the following array, which is pre-populated in your solution:
+
+  ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  
+  return birds.filter(i => !geese.includes(i));
+};
+*/
