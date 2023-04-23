9524bf679bc71fee70f98a906aa8650e9fe30470
Justin Newton <justinmnewton25@gmail.com>
2023-04-23 09:28:13 -0500

    Remove the time

--- practice.js
+++ practice.js
@@ -1934,3 +1934,18 @@ function validateHello(greetings) {
   return /(ahoj|ciao|czesc|h[ae]llo|hola|salut)/i.test(greetings);
 }
 */
+
+/* Remove the time
+
+You're re-designing a blog, and the blog's posts have the Weekday Month Day, time format for showing 
+the date and time when a post was made, e.g., Friday May 2, 7pm.
+
+You're running out of screen real estate, and on some pages you want to display a shorter format, 
+Weekday Month Day that omits the time.
+
+Write a function that takes the website date/time in its original string format and returns the shortened format.
+
+function shortenToDate(longDate) {
+  return longDate.split(",")[0];
+}
+*/
