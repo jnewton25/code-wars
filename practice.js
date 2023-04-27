1e84a5216e958005c5434be7cf5bdda9a5108408
Justin Newton <justinmnewton25@gmail.com>
2023-04-27 08:10:28 -0500

    Price of mangoes

--- practice.js
+++ practice.js
@@ -1976,3 +1976,13 @@ Add the value "codewars" to the array websites 1,000 times.
 
 var websites = new Array(1000).fill("codewars");
 */
+
+/* Price of Mangoes
+
+There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), 
+calculate the total cost of the mangoes.
+
+function mango(quantity, price){
+  return (quantity - (Math.floor(quantity / 3))) * price;
+}
+*/
