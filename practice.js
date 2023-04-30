d5a4c291d59d6321268b9da8585c34f7962ee68c
Justin Newton <justinmnewton25@gmail.com>
2023-04-30 07:11:37 -0500

    Pythagorean triple

--- practice.js
+++ practice.js
@@ -2013,3 +2013,19 @@ function mystery() {
   return results;
 }
 */
+
+/* Pythagorean Triple
+
+Given an unsorted array of 3 positive integers [ n1, n2, n3 ], determine if it is possible to form 
+a Pythagorean Triple using those 3 integers.
+
+A Pythagorean Triple consists of arranging 3 integers, such that:
+
+a2 + b2 = c2
+
+function isPythagoreanTriple(nums) {
+  const [a,b,c] = nums.sort((a,b) => a - b)
+
+  return a**2 + b**2 === c**2
+}
+*/
