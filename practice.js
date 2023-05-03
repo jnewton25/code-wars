9c11d20d41868bb9bc109abf48b75cd599f16341
Justin Newton <justinmnewton25@gmail.com>
2023-05-03 05:53:06 -0500

    Messi goals

--- practice.js
+++ practice.js
@@ -2051,3 +2051,14 @@ function addFive(num) {
   return total;
 }
 */
+
+/* Grasshopper - Messi Goals
+
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+*/
