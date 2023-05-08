aee748de643f7a89eed9fe3de28887ae310e18fd
Justin Newton <justinmnewton25@gmail.com>
2023-05-08 16:38:58 -0500

    Regular ball

--- practice.js
+++ practice.js
@@ -2062,3 +2062,14 @@ var copaDelReyGoals = 5;
 
 var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
 */
+
+/* Regular Ball Super Ball
+
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+var Ball = function(ballType) {
+  this.ballType = ballType || 'regular';
+};
+*/
