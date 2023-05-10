e79a69b8d89382d538688dd9a45e4fb55e0f4380
Justin Newton <justinmnewton25@gmail.com>
2023-05-10 08:58:54 -0500

    String templates - bug fixing #5

--- practice.js
+++ practice.js
@@ -2117,3 +2117,13 @@ function switchItUp(number){
 }
 }
 */
+
+/* String Templates - Bug Fixing #5
+
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String 
+Template feature, Help Timmy with his string template so it works as he expects!
+
+function buildString(...template){
+  return `I like ${template.join(', ')}!`;
+}
+*/
