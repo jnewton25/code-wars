42ab7d396568bc63ea9f9688d8ffd1d3f199817c
Justin Newton <justinmnewton25@gmail.com>
2023-05-16 09:51:18 -0500

    Super duper easy

--- practice.js
+++ practice.js
@@ -2214,3 +2214,13 @@ function greet(language) {
 }
 }
 */
+
+/* Super Duper Easy
+
+Make a function that returns the value multiplied by 50 and increased by 6. 
+If the value entered is a string it should return "Error".
+
+function problem(x){
+  return typeof(x) !== "string" ? x * 50 + 6 : "Error";
+}
+*/
