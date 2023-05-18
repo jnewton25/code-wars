a7f33205dc058534bad5618986ffbcaaf478dbc6
Justin Newton <justinmnewton25@gmail.com>
2023-05-18 09:46:22 -0500

    Terminal game move function

--- practice.js
+++ practice.js
@@ -2224,3 +2224,16 @@ function problem(x){
   return typeof(x) !== "string" ? x * 50 + 6 : "Error";
 }
 */
+
+/* Grasshopper - Terminal game move function
+
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces 
+indicated by the dice two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and 
+return the new position.
+
+function move (position, roll) {
+  return position + roll * 2;
+}
+*/
