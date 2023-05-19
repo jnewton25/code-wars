302e081cdf28bb0621fbba60962e9809dd70da6c
Justin Newton <justinmnewton25@gmail.com>
2023-05-19 09:22:43 -0500

    Expressions matter

--- practice.js
+++ practice.js
@@ -2237,3 +2237,21 @@ function move (position, roll) {
   return position + roll * 2;
 }
 */
+
+/* Expressions Matter
+
+Given three integers a ,b ,c, return the largest number obtained after inserting the following 
+operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained 
+(Read the notes for more detail about it)
+
+function expressionMatter(a, b, c) {
+  let output = [];
+  output.push(a *(b + c));
+  output.push(a * b * c);
+  output.push(a + b * c);
+  output.push((a + b) * c);
+  output.push(a+b+c);
+    return Math.max(...output);
+}
+*/
