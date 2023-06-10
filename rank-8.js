a8f56db4b15add862eba04d1737033ab64e327c2
Justin Newton <justinmnewton25@gmail.com>
2023-06-09 23:19:46 -0500

    Methods of string object

--- rank-8.js
+++ rank-8.js
@@ -68,4 +68,15 @@ function nameShuffler(str){
 }
 */
 
-/* */
+/* Training JS #18: Methods of String object--concat() split() and its good friend join()
+
+Implement a function which accepts 2 arguments: string and separator.
+
+The expected algorithm: split the string into words by spaces, split each word into 
+separate characters and join them back with the specified separator, join all the 
+resulting "words" back into a sentence with spaces.
+
+function splitAndMerge(str, sp) {
+  return str.split(" ").map(word => word.split("").join(sp)).join(" ");
+}
+*/
